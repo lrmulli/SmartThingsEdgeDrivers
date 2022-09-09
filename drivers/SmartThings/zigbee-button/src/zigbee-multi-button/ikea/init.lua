21ca5b6fb4e0ab7976e56d478d9f7da3f91709be
lrmulli <lmullineux@gmail.com>
2022-09-09 12:05:21 +0100

    removed prints

--- drivers/SmartThings/zigbee-button/src/zigbee-multi-button/ikea/init.lua
+++ drivers/SmartThings/zigbee-button/src/zigbee-multi-button/ikea/init.lua
@@ -69,11 +69,11 @@ local function zdo_binding_table_handler(driver, device, zb_rx)
   local groups = ""
   local devicebinds = ""
   for _, binding_table in pairs(zb_rx.body.zdo_body.binding_table_entries) do
-    print("Zigbee Group is:"..binding_table.dest_addr.value)
+    --print("Zigbee Group is:"..binding_table.dest_addr.value)
     if binding_table.dest_addr_mode.value == binding_table.DEST_ADDR_MODE_SHORT then
       -- send add hub to zigbee group command
       driver:add_hub_to_zigbee_group(binding_table.dest_addr.value)
-      print("Adding to zigbee group: "..binding_table.dest_addr.value)
+      --print("Adding to zigbee group: "..binding_table.dest_addr.value)
       groups = groups..binding_table.cluster_id.value.."("..binding_table.dest_addr.value.."),"
     else
       driver:add_hub_to_zigbee_group(0x0000)
